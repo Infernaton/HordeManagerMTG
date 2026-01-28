df0ed9e0fbfb8cdcba2f67c25396b24df1b2b68b
Infernaton <lubin.pauly@gmail.com>
2026-01-28 18:51:09 +0100

    feat: front begining

---
+++ horde-manager-mtg/src/app/pages/App.js
@@ -0,0 +1,16 @@
+// import logo from "../../public/logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Deck List</p>
+                <hr />
+            </header>
+            <div className="App-body">TEST</div>
+        </div>
+    );
+}
+
+export default App;

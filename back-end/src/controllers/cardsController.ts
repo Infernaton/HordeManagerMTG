384fbd9eff630aaa9001553aac490fe623a27be3
Infernaton <lubin.pauly@gmail.com>
2026-01-28 19:23:15 +0100

    feat: fetch card data from scryfall api

--- back-end/src/controllers/cardsController.ts
+++ back-end/src/controllers/cardsController.ts
@@ -1,9 +1,12 @@
+import axios from "axios";
 import type { Request, Response }  from "express";
 
 export const getCardData = async (req: Request, res: Response) => {
   try {
-    res.status(200).json({message: "success"});
+    const fetch = await axios.get(`https://api.scryfall.com/cards/` + req.params["id"]);
+    res.status(200).json(fetch.data);
   } catch (error) {
-      res.status(404).json({ message: "fail" });
+    console.log(error);
+    res.status(404).json({ message: "fail fetching card data" });
   }
 }

c0068583b50106d8155156c2e49e03b2ccebfdf5
Infernaton <lubin.pauly@gmail.com>
2026-02-25 11:06:10 +0100

    feat: display card image
    - get unique card from list
    - count occurence of the card appearance in the deck
    - need css

--- horde-manager-mtg/src/app/components/CardList.tsx
+++ horde-manager-mtg/src/app/components/CardList.tsx
@@ -1,10 +1,38 @@
 import { Card } from "../models/Card";
 
+interface CardDisplay {
+	id: string;
+	occurence: number;
+	card_data: Card;
+}
+
 function CardList({ cardList }: { cardList: Card[] }) {
+	let uniqueCard: CardDisplay[] = [];
+	for (let i = 0; i < cardList.length; i++) {
+		const currentCardIndex = uniqueCard.findIndex((u) => u.id == cardList[i].id);
+		if (currentCardIndex != -1) {
+			uniqueCard[currentCardIndex].occurence++;
+		} else {
+			uniqueCard.push({
+				id: cardList[i].id,
+				occurence: 1,
+				card_data: cardList[i],
+			});
+		}
+	}
 	return (
 		<div>
 			<div className="card-container col7">
-				{cardList && cardList.map((cardObj) => <div key={cardObj.id}>{cardObj.front_card.name}</div>)}
+				{uniqueCard.length > 0 &&
+					uniqueCard.map((cardObj) => (
+						<div key={cardObj.id}>
+							<img
+								src={cardObj.card_data.front_card.full_image.toString()}
+								alt={cardObj.card_data.front_card.name}
+							/>
+							{cardObj.card_data.front_card.name} //// {cardObj.occurence}
+						</div>
+					))}
 			</div>
 		</div>
 	);

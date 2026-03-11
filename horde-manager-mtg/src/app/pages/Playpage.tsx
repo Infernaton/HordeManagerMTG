1a70474c1edd1474c0299b80dbc04fd6cd6094b5
Infernaton <72390824+Infernaton@users.noreply.github.com>
2026-03-11 15:22:16 +0100

    feat: alert the user if he try to unload the play page
    to prevent some bad interaction

--- horde-manager-mtg/src/app/pages/Playpage.tsx
+++ horde-manager-mtg/src/app/pages/Playpage.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import BattleField from "../components/BattleField";
 import { Deck } from "../models/Deck";
 import { Store } from "../store";
 
 function Playpage() {
 	const currentDeck = Store.Local.getObject("currentDeck") as Deck;
+
+	useEffect(() => {
+		window.addEventListener("beforeunload", alertUser);
+		return () => {
+			window.removeEventListener("beforeunload", alertUser);
+		};
+	}, []);
+	const alertUser = (e: Event) => {
+		e.preventDefault();
+	};
 
 	console.log(currentDeck);
 	return (
